9258b05bce52570a287e062dd60c8626a3936797
stedyyulius <stedyyulius@gmail.com>
2018-03-25 12:00:07 +0700

    todolist dolo

--- ecommerce/src/App.js
+++ ecommerce/src/App.js
@@ -1,18 +1,63 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state= {
+      todos: [],
+      task: '',
+      edit: ''
+    }
+  }
+
+
+  addTodo(e) {
+    if (e === 'Enter') {
+      let todo = this.state.todos;
+      todo.push(this.state.task);
+      this.setState({
+        todos: todo
+      })
+      console.log(this.refs)
+      this.refs.task.value = '';
+    }
+  }
+
+  deleteTodo(i) {
+    let todo = this.state.todos
+    todo.splice(i,1)
+    this.setState({
+      todos: todo
+    })
+  }
+
+  editTodo(value, i) {
+    let todo = this.state.todos
+    todo[i] = value
+    this.setState({
+      todos: todo
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Todo tes</h1>
+        <input
+          onChange={(e)=> this.setState({task:e.target.value})}
+          type="text" onKeyPress={(e)=> this.addTodo(e.key)}
+          ref="task"/>
+        {this.state.todos.map((todo,i)=>
+          <div key={i}>
+            <p onClick={()=> this.setState({edit:i})}>{todo} <button onClick={()=>this.deleteTodo(i)}>X</button></p>
+            {(this.state.edit === i)
+              ? <input onChange={(e)=> this.editTodo(e.target.value, i)} type="text" />
+              : null
+            }
+          </div>
+        )}
+        {/* <button onClick={()=> this.addTodo()}>Add todo</button> */}
       </div>
     );
   }
